e85afa5188bf3afb5ac56a91b324542bd94dcb7c
ahanaf <ahanafabdullah9@gmail.com>
2022-11-04 00:35:17 +0600

    added page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/layouts/Footer";
+import Navbar from "./components/layouts/Navbar";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen ">
         <Navbar />
+        <main className="container mx-auto px-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
